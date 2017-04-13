0bdcace06f37de889b3a6583faf8f4183b9ebe13
Michael Notarnicola <michael@beardphantom.com>
2017-04-13 02:11:11 -0400

    Complete rewrite.

---
+++ Assets/UConsole/Scripts/DevConsoleCommandAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BeardPhantom.UConsole
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    public sealed class DevConsoleCommandAttribute : Attribute
+    {
+        public string description;
+        public string[] aliases;
+
+        public DevConsoleCommandAttribute(string description, params string[] aliases)
+        {
+            this.description = description;
+            this.aliases = aliases;
+        }
+    }
+}
